35c33503508fe1b18286416ae4fd4e6b89f04062
ssachio <sachio@SACHIO-DELL.localdomain>
2020-07-09 17:42:29 +0900

    initial commit

---
+++ src/test/java/jp/kobespiral/agiledev/AgiledevApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.kobespiral.agiledev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgiledevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
