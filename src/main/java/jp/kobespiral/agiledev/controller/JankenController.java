f7e25658c40d8a0724be92a193e9c0fd090ce13d
Hiroshi Igaki <github@musictale.net>
2020-07-10 13:38:06 +0900

    Basicじゃんけん

--- src/main/java/jp/kobespiral/agiledev/controller/JankenController.java
+++ src/main/java/jp/kobespiral/agiledev/controller/JankenController.java
@@ -1,5 +1,7 @@
 package jp.kobespiral.agiledev.controller;
 
+import java.util.Random;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,16 +15,46 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 @RequestMapping("janken")
 public class JankenController {
+  Random rand = new Random();
 
   @PostMapping
-  public String janken(@RequestParam("hand") String hand, ModelMap model) {
-    model.addAttribute("cpuHand", hand);
+  public String janken(@RequestParam("hand") final String hand, final ModelMap model) {
+    model.addAttribute("playerHand", hand);
+    String cpuHand = this.getCpuHand();
+    model.addAttribute("cpuHand", cpuHand);
+    model.addAttribute("winner", getWinner(hand, cpuHand));
+
     return "janken.html";
   }
 
   @GetMapping
   public String janken() {
     return "janken.html";
+  }
+
+  private String getWinner(String playerHand, String cpuHand) {
+    if (playerHand.equals(cpuHand)) {
+      return "Draw";
+    } else if (playerHand.equals("gu") && cpuHand.equals("choki") || playerHand.equals("choki") && cpuHand.equals("pa")
+        || playerHand.equals("pa") && cpuHand.equals("gu")) {
+      return "Player";
+    }
+    return "CPU";
+  }
+
+  private String getCpuHand() {
+    final int cpuHand = rand.nextInt(3);
+    switch (cpuHand) {
+      case 0:
+        return "gu";
+      case 1:
+        return "choki";
+      case 2:
+        return "pa";
+    }
+    System.out.println(cpuHand);
+    return "dummy";
+
   }
 
 }
