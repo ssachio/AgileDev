7f872cabf42622d435e377d3c1eaf075ad46c9df
Hiroshi Igaki <github@musictale.net>
2020-07-10 13:24:19 +0900

    GET/POSTでボタンでじゃんけんの手を選択する処理実装

--- src/main/java/jp/kobespiral/agiledev/controller/JankenController.java
+++ src/main/java/jp/kobespiral/agiledev/controller/JankenController.java
@@ -1,15 +1,26 @@
 package jp.kobespiral.agiledev.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * JankenController
  */
 @Controller
+@RequestMapping("janken")
 public class JankenController {
 
-  @RequestMapping("janken")
+  @PostMapping
+  public String janken(@RequestParam("hand") String hand, ModelMap model) {
+    model.addAttribute("cpuHand", hand);
+    return "janken.html";
+  }
+
+  @GetMapping
   public String janken() {
     return "janken.html";
   }
