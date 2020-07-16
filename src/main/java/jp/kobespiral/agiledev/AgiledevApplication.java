1030ce4d30b6691873b1ffdfc9c8d36d5ce8130e
Hiroshi Igaki <github@musictale.net>
2020-07-16 20:02:15 +0900

    AgileDev -> Agiledev

--- src/main/java/jp/kobespiral/agiledev/AgiledevApplication.java
+++ src/main/java/jp/kobespiral/agiledev/AgiledevApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AgileDevApplication {
+public class AgiledevApplication {
 
   public static void main(String[] args) {
-    SpringApplication.run(AgileDevApplication.class, args);
+    SpringApplication.run(AgiledevApplication.class, args);
   }
 
 }
