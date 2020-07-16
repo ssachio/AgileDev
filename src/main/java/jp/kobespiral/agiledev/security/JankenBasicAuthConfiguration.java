c94d689abf10450693ca8423911ac3a87fe63360
Hiroshi Igaki <github@musictale.net>
2020-07-16 17:52:07 +0900

    basic認証に変更

--- src/main/java/jp/kobespiral/agiledev/security/JankenBasicAuthConfiguration.java
+++ src/main/java/jp/kobespiral/agiledev/security/JankenBasicAuthConfiguration.java
@@ -1,10 +1,12 @@
 package jp.kobespiral.agiledev.security;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.logout.HttpStatusReturningLogoutSuccessHandler;
 
 /**
  * JankenBasicAuthConfiguration
@@ -21,7 +23,7 @@ public class JankenBasicAuthConfiguration extends WebSecurityConfigurerAdapter {
 
   @Override
   protected void configure(final HttpSecurity http) throws Exception {
-    http.authorizeRequests().antMatchers("/janken/**").authenticated().and().formLogin().and().logout();
+    http.authorizeRequests().antMatchers("/janken/**").authenticated().and().httpBasic();
     http.csrf().disable();// POSTリクエストが拒否されるのでdisableに．CSRF自体はOFFにするのは本来危険だが，今回は仮にということで
   }
 
